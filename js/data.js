53883729d19bb5025b47692cd52510b715dd12b2
stepe-ee <steve.anggana@gmail.com>
2026-06-14 16:57:33 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,27 @@
+const HP_LIST = [
+  { id:1,  brand:'Samsung', name:'Galaxy S24 Ultra',    type:'Flagship',  price:19999000 },
+  { id:2,  brand:'Samsung', name:'Galaxy S24 FE',       type:'Mid Range', price:8499000  },
+  { id:3,  brand:'Samsung', name:'Galaxy A55 5G',       type:'Mid Range', price:5799000  },
+  { id:4,  brand:'Samsung', name:'Galaxy A35 5G',       type:'Mid Range', price:4399000  },
+  { id:5,  brand:'Samsung', name:'Galaxy M35 5G',       type:'Entry',     price:3599000  },
+  { id:6,  brand:'iPhone',  name:'iPhone 16 Pro Max',   type:'Flagship',  price:25999000 },
+  { id:7,  brand:'iPhone',  name:'iPhone 16 Pro',       type:'Flagship',  price:21999000 },
+  { id:8,  brand:'iPhone',  name:'iPhone 16',           type:'Standard',  price:16999000 },
+  { id:9,  brand:'iPhone',  name:'iPhone 15',           type:'Standard',  price:13499000 },
+  { id:10, brand:'Xiaomi',  name:'14T Pro',             type:'Flagship',  price:8999000  },
+  { id:11, brand:'Xiaomi',  name:'Redmi Note 13 Pro',   type:'Mid Range', price:4199000  },
+  { id:12, brand:'Xiaomi',  name:'Poco X6 Pro',         type:'Mid Range', price:4799000  },
+  { id:13, brand:'Xiaomi',  name:'Redmi 13C',           type:'Entry',     price:1899000  },
+  { id:14, brand:'OPPO',    name:'Find X8 Pro',         type:'Flagship',  price:17999000 },
+  { id:15, brand:'OPPO',    name:'Reno 12 Pro',         type:'Mid Range', price:6999000  },
+  { id:16, brand:'OPPO',    name:'A60',                 type:'Entry',     price:2699000  },
+  { id:17, brand:'Vivo',    name:'X100 Pro',            type:'Flagship',  price:14999000 },
+  { id:18, brand:'Vivo',    name:'V40 Lite',            type:'Mid Range', price:3299000  },
+  { id:19, brand:'Realme',  name:'GT 6T',               type:'Flagship',  price:7999000  },
+  { id:20, brand:'Realme',  name:'C75',                 type:'Entry',     price:1999000  },
+];
+
+const TIER = {
+  silver: { label: 'Silver 12 Bln', price: 120000 },
+  gold:   { label: 'Gold 12 Bln',   price: 199000 },
+};
